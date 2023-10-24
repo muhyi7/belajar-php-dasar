0871a63c4af24187ae300be09345368ac2939f6e
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-10-24 18:07:40 +0700

    Variable Function

---
+++ VariableFunction.php
@@ -0,0 +1,29 @@
+<?php
+
+function foo(){
+    echo "Foo" . PHP_EOL;
+}
+
+function bar(){
+    echo "bar" . PHP_EOL;
+}
+
+$functionYangAkanDipanggil = "foo";
+$functionYangAkanDipanggil();
+
+$functionYangAkanDipanggil = "bar";
+$functionYangAkanDipanggil();
+
+function sayHello(string $name, $filter)
+{
+    $finalName = $filter($name);
+    echo "Hello $finalName" . PHP_EOL;
+}
+
+function sampleFunction(string $name): string {
+    return "Sample $name";
+}
+
+sayHello("Eko","sampleFunction");
+sayHello("Eko","strtoupper");
+sayHello("Eko","strtolower");
