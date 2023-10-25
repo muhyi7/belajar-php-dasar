b2aef5037af8d4d406f74c1a52f3092ce613e856
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-10-25 21:27:44 +0700

    Array Function

---
+++ ArrayFunction.php
@@ -0,0 +1,16 @@
+<?php
+
+$data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+$dataResult = array_map(fn(int $value) => $value * 10, $data);
+var_dump($dataResult);
+
+rsort($data);
+var_dump($data);
+
+$person = [
+    "first_name" => "Eko",
+    "last_name" => "Khannedy"
+];
+var_dump(array_keys($person));
+var_dump(array_values($person));
